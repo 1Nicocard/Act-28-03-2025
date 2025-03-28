40b01208fb32754d1cac62c1d1611d2cac4e173b
Nicolas Cardenas Guaca <1109187838@u.icesi.edu.co>
2025-03-28 17:58:37 -0500

    ñañañ

--- Act-28-03-25/actividad3.js
+++ Act-28-03-25/actividad3.js
@@ -1,6 +1,5 @@
-let nombres = ("Carlos", "Ana", "Pedro", "liliana", "sorriana")
-
-console.log (nombres)
+let nombres = ["Carlos", "Ana", "Pedro", "liliana", "sorriana"]
+console.log(nombres)
 
 let ordenados = nombres.sort()
 console.log(ordenados)
